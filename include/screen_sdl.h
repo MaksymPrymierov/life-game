126104b4a2c29e2262bc2752fd4f83242e464ed6
Maksym Prymierov <primerovmax@gmail.com>
2022-01-28 17:25:40 +0200

    Add screen_sdl game screen which workis via SDL library
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- include/screen_sdl.h
+++ include/screen_sdl.h
@@ -15,11 +15,30 @@ class screen_sdl : public screen {
   void show() override;
 
  private:
-  const int SCREEN_WIDTH = 1280;
-  const int SCREEN_HEIGHT = 720;
+  struct color {
+    int r;
+    int g;
+    int b;
+  };
 
-  SDL_Surface* screen_surface;
+  const unsigned int SCREEN_WIDTH = 1280;
+  const unsigned int SCREEN_HEIGHT = 720;
+  const unsigned int CELL_WIDTH = 20;
+  const unsigned int CELL_HEIGHT = 20;
+  const unsigned int GAME_SCREEN_WIDTH = 64;
+  const unsigned int GAME_SCREEN_HEIGHT = 36;
+  const unsigned int GAME_LIFE_PROBABILITY = 25;
+
+  const color dead_color = {0, 153, 153};
+  const color life_color = {255, 135, 0};
+
+  SDL_Surface* background_surface;
   SDL_Window* window;
+  SDL_Rect* cell;
+
+  void set_background();
+  void update();
+  void set_cell(unsigned int x, unsigned int y, color c);
 };
 
 }  // namespace game_life
--- src/screen_sdl.cpp
+++ src/screen_sdl.cpp
@@ -8,7 +8,12 @@ namespace game_life {
 
 screen_sdl::screen_sdl(int w, int h, int life_prob) : screen(w, h, life_prob) {}
 
-screen_sdl::~screen_sdl() {}
+screen_sdl::~screen_sdl() {
+  SDL_DestroyWindow(window);
+  SDL_Quit();
+
+  delete cell;
+}
 
 void screen_sdl::start() {
   if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
@@ -18,8 +23,60 @@ void screen_sdl::start() {
   window = SDL_CreateWindow("Life Game", SDL_WINDOWPOS_UNDEFINED,
                             SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH,
                             SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+
+  if (!window) {
+    fmt::print(fg(fmt::color::red), "Error: SDL Window can't create!");
+  }
+
+  background_surface = SDL_GetWindowSurface(window);
+
+  m_width = GAME_SCREEN_WIDTH;
+  m_height = GAME_SCREEN_HEIGHT;
+  m_life_probability = GAME_LIFE_PROBABILITY;
+
+  m_screen_map.resize(m_height);
+  for (auto &i : m_screen_map) {
+    i.resize(static_cast<std::size_t>(m_width));
+  }
+  random_map_set();
+
+  cell = new SDL_Rect(0, 0, CELL_WIDTH, CELL_HEIGHT);
+
+  set_background();
 }
 
-void screen_sdl::show() {}
+void screen_sdl::show() {
+  for (int i = 0; i < m_height; ++i) {
+    for (int j = 0; j < m_width; ++j) {
+      if (m_screen_map.at(i).at(j)) {
+        set_cell(j, i, life_color);
+      } else {
+        set_cell(j, i, dead_color);
+      }
+    }
+  }
+
+  update();
+}
+
+void screen_sdl::set_background() {
+  SDL_FillRect(background_surface, NULL,
+               SDL_MapRGB(background_surface->format, dead_color.r,
+                          dead_color.g, dead_color.b));
+}
+
+void screen_sdl::update() { SDL_UpdateWindowSurface(window); }
+
+void screen_sdl::set_cell(unsigned int x, unsigned int y, color c) {
+  if (x >= GAME_SCREEN_WIDTH || y >= GAME_SCREEN_HEIGHT) {
+    return;
+  }
+
+  cell->x = x * CELL_WIDTH;
+  cell->y = y * CELL_HEIGHT;
+
+  SDL_FillRect(background_surface, cell,
+               SDL_MapRGB(background_surface->format, c.r, c.g, c.b));
+}
 
 }  // namespace game_life
