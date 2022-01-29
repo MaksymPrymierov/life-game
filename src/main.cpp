5383f38aa89b60175a17902aede7cf371e36f6c2
Maksym Prymierov <primerovmax@gmail.com>
2022-01-29 20:03:21 +0200

    Parse command line arguments with the boost::program_options library
    Signed-off-by: Maksym Prymierov <primerovmax@gmail.com>

--- src/main.cpp
+++ src/main.cpp
@@ -5,81 +5,47 @@
 #include <game.h>
 #include <screen/factory.h>
 
-void help() {
-  fmt::print(fg(fmt::color::yellow), "Usage: ./GameLife [options]\n");
-  fmt::print("Options:\n");
-  fmt::print(
-      "  -c --cli\tStart the game with default command line interface\n");
-  fmt::print("  -h --help\tPrint the help page\n");
-  fmt::print(
-      "  -n --ncurses\tStart the game with ncurses command line interface\n");
-  fmt::print("  -s --sdl\tStart the game with sdl graphic library\n");
-  fmt::print("  -v --version\tPrint version\n");
+#include <boost/program_options.hpp>
+#include <iostream>
 
-  fmt::print(
-      "\nReport bugs to "
-      "<https://github.com/MaksymPrymierov/life-game/issues>\n");
-}
+namespace po = boost::program_options;
 
 int main(int argc, char *argv[]) {
   std::unique_ptr<game_life::screen> screen;
   std::unique_ptr<game_life::game> game;
   game_life::screen_factory scr_factory;
-  std::vector<std::string> args;
-  bool is_game_screen = false;
 
-  for (int i = 1; i < argc; i++) {
-    args.push_back(argv[i]);
-  }
+  po::variables_map vm;
+  po::options_description desc("Usage: ./GameLife [options]");
+  std::string screen_type = "sdl";
 
-  if (argc == 1) {
-    screen = std::move(scr_factory.get("sdl"));
-    is_game_screen = true;
-  }
+  desc.add_options()("help,h", "Show the help page")(
+      "screen,s", po::value<std::string>(&screen_type),
+      "Select the game screen: cli, ncurses, sdl (default sdl)")(
+      "version,v", "Print program version");
 
-  for (const auto &i : args) {
-    if (i == "--help" || i == "-h") {
-      help();
-    } else if (i == "--cli" || i == "-c") {
-      if (!is_game_screen) {
-        is_game_screen = true;
-        screen = std::move(scr_factory.get("cli"));
-      } else {
-        fmt::print(fg(fmt::color::red),
-                   "Error: You can choose only one game screen\n");
-        return -1;
-      }
-    } else if (i == "--ncurses" || i == "-n") {
-      if (!is_game_screen) {
-        is_game_screen = true;
-        screen = std::move(scr_factory.get("ncurses"));
-      } else {
-        fmt::print(fg(fmt::color::red),
-                   "Error: You can choose only one game screen\n");
-        return -1;
-      }
-    } else if (i == "--sdl" || i == "-s") {
-      if (!is_game_screen) {
-        is_game_screen = true;
-        screen = std::move(scr_factory.get("sdl"));
-      } else {
-        fmt::print(fg(fmt::color::red),
-                   "Error: You can choose only one game screen\n");
-        return -1;
-      }
-    } else if (i == "--version" || i == "-v") {
-      fmt::print("Game Life Version: {}.{}\n", GameLife_VERSION_MAJOR,
-                 GameLife_VERSION_MINOR);
-    } else {
-      fmt::print(fg(fmt::color::red),
-                 "Error: Unknow argument {}. Try to use --help argument.\n", i);
-    }
-  }
+  po::parsed_options parsed = po::command_line_parser(argc, argv)
+                                  .options(desc)
+                                  .allow_unregistered()
+                                  .run();
+  po::store(parsed, vm);
+  po::notify(vm);
 
-  if (!is_game_screen) {
+  if (vm.count("help")) {
+    std::cout << desc << std::endl;
+    fmt::print(
+        "\nReport bugs to "
+        "<https://github.com/MaksymPrymierov/life-game/issues>\n");
     return 0;
   }
 
+  if (vm.count("version")) {
+    fmt::print("Game Life Version: {}.{}\n", GameLife_VERSION_MAJOR,
+               GameLife_VERSION_MINOR);
+    return 0;
+  }
+
+  screen = std::move(scr_factory.get(screen_type));
   screen->start();
   game = std::make_unique<game_life::game>(screen);
   game->start();
